fd6d8505aa2f40b436fd9e78f4ae3a52c2aa026a
unhourin <59089824+unhourin@users.noreply.github.com>
2020-05-15 13:56:24 +0900

    フレームワーク
    新たなSpringBoot

---
+++ src/main/java/com/tomato/syunnamori/SyunnamoriApplication.java
@@ -0,0 +1,13 @@
+package com.tomato.syunnamori;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyunnamoriApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SyunnamoriApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tomato/syunnamori/SyunnamoriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tomato.syunnamori;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyunnamoriApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
