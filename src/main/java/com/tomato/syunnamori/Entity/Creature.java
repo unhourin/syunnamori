6185213a969c863e99271b72de6c6e2fb3f924cc
ghkddlrwn <tomato.hwangij@gmail.com>
2020-05-19 09:50:39 +0900

    Creatureクラスを作成

--- src/main/java/com/tomato/syunnamori/Entity/Creature.java
+++ src/main/java/com/tomato/syunnamori/Entity/Creature.java
@@ -2,4 +2,82 @@ package com.tomato.syunnamori.Entity;
 
 public class Creature {
 
+	private String C_CODE;
+	private String C_NAME;
+	private int C_PRICE;
+	private String S_NAME;
+	private String IMG;
+	private String C_PLACE;
+	private String F_SH;
+	private int N_TIME;
+	private int S_TIME;
+	private int D_FLG;
+	/*
+	VER
+	LUD_DATE
+	LUD_PERSON
+	FUD_DATE
+	FUD_PERSON
+	 */
+
+	public String getC_CODE() {
+		return C_CODE;
+	}
+	public void setC_CODE(String c_CODE) {
+		C_CODE = c_CODE;
+	}
+	public String getC_NAME() {
+		return C_NAME;
+	}
+	public void setC_NAME(String c_NAME) {
+		C_NAME = c_NAME;
+	}
+	public int getC_PRICE() {
+		return C_PRICE;
+	}
+	public void setC_PRICE(int c_PRICE) {
+		C_PRICE = c_PRICE;
+	}
+	public String getS_NAME() {
+		return S_NAME;
+	}
+	public void setS_NAME(String s_NAME) {
+		S_NAME = s_NAME;
+	}
+	public String getIMG() {
+		return IMG;
+	}
+	public void setIMG(String iMG) {
+		IMG = iMG;
+	}
+	public String getC_PLACE() {
+		return C_PLACE;
+	}
+	public void setC_PLACE(String c_PLACE) {
+		C_PLACE = c_PLACE;
+	}
+	public String getF_SH() {
+		return F_SH;
+	}
+	public void setF_SH(String f_SH) {
+		F_SH = f_SH;
+	}
+	public int getN_TIME() {
+		return N_TIME;
+	}
+	public void setN_TIME(int n_TIME) {
+		N_TIME = n_TIME;
+	}
+	public int getS_TIME() {
+		return S_TIME;
+	}
+	public void setS_TIME(int s_TIME) {
+		S_TIME = s_TIME;
+	}
+	public int getD_FLG() {
+		return D_FLG;
+	}
+	public void setD_FLG(int d_FLG) {
+		D_FLG = d_FLG;
+	}
 }
