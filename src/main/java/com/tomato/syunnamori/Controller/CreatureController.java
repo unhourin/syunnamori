c1f14e6ac3bddf73cd3d3d0616ce63378ce9f1bc
unhourin <59089824+unhourin@users.noreply.github.com>
2020-05-22 16:44:19 +0900

    初期化にする

--- src/main/java/com/tomato/syunnamori/Controller/CreatureController.java
+++ src/main/java/com/tomato/syunnamori/Controller/CreatureController.java
@@ -6,30 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.tomato.syunnamori.Dto.AjaxResult;
-import com.tomato.syunnamori.Service.CreatureService;
 
 @RestController
 public class CreatureController {
-	
-	@Autowired
-	private CreatureService service;
 
-	// リアルタイム画面のapi
-	//
-	@GetMapping("/riarutaimu")
-	public AjaxResult Riarutaimu() {
-		return service.queryByNowTime();
-	}
-
-	//今月画面のapi
-	@GetMapping("/kongetsu")
-	public AjaxResult Kongetsu() {
-		return service.queryByThisMonth();
-	}
-
-	//全表示画面のapi
-	@GetMapping("/zenhyouji")
-	public AjaxResult zenhyouji() {
-		return service.queryAll();
-	}
 }
