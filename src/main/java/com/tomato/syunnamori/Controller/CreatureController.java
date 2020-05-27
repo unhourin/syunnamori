7d856f590cb7360059fcf7e3ad88054474913a12
unhourin <59089824+unhourin@users.noreply.github.com>
2020-05-27 12:39:27 +0900

    ファイルを整理した

--- src/main/java/com/tomato/syunnamori/Controller/CreatureController.java
+++ src/main/java/com/tomato/syunnamori/Controller/CreatureController.java
@@ -1,7 +1,10 @@
 package com.tomato.syunnamori.Controller;
 
 
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -39,4 +42,36 @@ public class CreatureController {
 			return new AjaxResult(0,"NULL");
 		return new AjaxResult(1,"SUCCESS",list);
 	}
+	
+//	@GetMapping("/kongetsu")
+//	public AjaxResult zenhyouji() {
+//
+//		List<Creature> list = creatureDao.queryAll();
+//
+//		if (list.isEmpty() || list == null)
+//			return new AjaxResult(0, "NULL");
+//
+//		Calendar nowCalendar = Calendar.getInstance();
+//		int thisMonth = nowCalendar.get(Calendar.MONTH) + 1;
+//		int thisHour = nowCalendar.get(Calendar.HOUR_OF_DAY);
+//
+//		List<Creature> specList = list.stream().filter(c -> c.getMonth().getStart_month() > c.getMonth().getEnd_month())
+//				.collect(Collectors.toList());
+//		List<Creature> newSpecList = specList.stream()
+//				.filter(c -> (c.getMonth().getStart_month() < thisMonth && thisMonth < 12)
+//						|| (0 < thisMonth && thisMonth < c.getMonth().getEnd_month()))
+//				.collect(Collectors.toList());
+//		List<Creature> comcList = list.stream().filter(c -> c.getMonth().getStart_month() < c.getMonth().getEnd_month())
+//				.collect(Collectors.toList());
+//		List<Creature> newComcList = list.stream()
+//				.filter(c -> c.getMonth().getStart_month() < thisMonth && thisMonth < c.getMonth().getEnd_month())
+//				.collect(Collectors.toList());
+//
+//		List<Creature> tmpList = new ArrayList<Creature>();
+//		tmpList.addAll(newComcList);
+//		tmpList.addAll(newSpecList);
+//
+//		return new AjaxResult(1, "SUCCESS", tmpList);
+//
+//	}
 }
