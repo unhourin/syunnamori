d004bdadadda84ade7488ec3a7c41e8375a02c9a
lklk1024 <32770832+lklk1024@users.noreply.github.com>
2020-05-27 09:18:31 +0900

    Add files via upload

---
+++ src/main/java/com/tomato/syunnamori/Dao/CreatureRealTimeDao.java
@@ -0,0 +1,12 @@
+package com.tomato.syunnamori.Dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.tomato.syunnamori.Entity.Creature;
+@Mapper
+public interface CreatureRealTimeDao {
+
+	List<Creature> queryRealTime();
+}
