5e675c65cf4051d60a70e1691c384c1f3d883244
unhourin <59089824+unhourin@users.noreply.github.com>
2020-05-27 14:51:59 +0900

    コメント追加（雲）

--- src/main/java/com/tomato/syunnamori/Controller/CreatureController.java
+++ src/main/java/com/tomato/syunnamori/Controller/CreatureController.java
@@ -20,14 +20,23 @@ public class CreatureController {
 	@Autowired
 	private CreatureDao creatureDao;
 
+	
+	//作成者：雲鳳麟
+	//全表示画面のAPI
 	@GetMapping("/zenhyouji")
 	public AjaxResult zenhyouji() {
+		// すべとのデータをCreatureDaoから取得
 		List<Creature> list =creatureDao.queryAll();
+		
+		//ヌロかどうかの確認
 		if(list.isEmpty()||list==null)
+			//であったら　messageを戻す
 			return new AjaxResult(0,"NULL");
 		return new AjaxResult(1,"SUCCESS",list);
 
 	}
+	
+	
 	@GetMapping("/kongetsu")
 	public AjaxResult kongetsu() {
 		List<Creature> list = creatureDao.queryThisMonth();
@@ -35,6 +44,9 @@ public class CreatureController {
 			return new AjaxResult(0,"NULL");
 		return new AjaxResult(1,"SUCCESS",list);
 	}
+	
+	
+	
 	@GetMapping("/riarutaimu")
 	public AjaxResult riarutaimu() {
 		List<Creature> list = creatureDao.queryRealTime();
