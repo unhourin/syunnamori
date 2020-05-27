e8f8b4af06ac72c507a8377732eccefc32502df1
lklk1024 <32770832+lklk1024@users.noreply.github.com>
2020-05-27 15:11:24 +0900

    Add files via upload

--- src/main/java/com/tomato/syunnamori/Controller/CreatureController.java
+++ src/main/java/com/tomato/syunnamori/Controller/CreatureController.java
@@ -1,3 +1,4 @@
+  
 package com.tomato.syunnamori.Controller;
 
 
@@ -47,46 +48,16 @@ public class CreatureController {
 		//成功の場合はlistを戻す
 		return new AjaxResult(1,"SUCCESS",list);
 	}
-
-
-
-	@GetMapping("/riarutaimu")
-	public AjaxResult riarutaimu() {
+	//作成者=>楊炯
+	//現在時間表現のAPI
+	@GetMapping("/realtime")
+	public AjaxResult realTime() {
+		//必要なデータをcreatureDaoから取得
 		List<Creature> list = creatureDao.queryRealTime();
+		//取得成功したかどうかを確認
 		if(list.isEmpty() || list ==null)
+			//失敗したら、メッセージを戻す
 			return new AjaxResult(0,"NULL");
 		return new AjaxResult(1,"SUCCESS",list);
 	}
-
-//	@GetMapping("/kongetsu")
-//	public AjaxResult zenhyouji() {
-//
-//		List<Creature> list = creatureDao.queryAll();
-//
-//		if (list.isEmpty() || list == null)
-//			return new AjaxResult(0, "NULL");
-//
-//		Calendar nowCalendar = Calendar.getInstance();
-//		int thisMonth = nowCalendar.get(Calendar.MONTH) + 1;
-//		int thisHour = nowCalendar.get(Calendar.HOUR_OF_DAY);
-//
-//		List<Creature> specList = list.stream().filter(c -> c.getMonth().getStart_month() > c.getMonth().getEnd_month())
-//				.collect(Collectors.toList());
-//		List<Creature> newSpecList = specList.stream()
-//				.filter(c -> (c.getMonth().getStart_month() < thisMonth && thisMonth < 12)
-//						|| (0 < thisMonth && thisMonth < c.getMonth().getEnd_month()))
-//				.collect(Collectors.toList());
-//		List<Creature> comcList = list.stream().filter(c -> c.getMonth().getStart_month() < c.getMonth().getEnd_month())
-//				.collect(Collectors.toList());
-//		List<Creature> newComcList = list.stream()
-//				.filter(c -> c.getMonth().getStart_month() < thisMonth && thisMonth < c.getMonth().getEnd_month())
-//				.collect(Collectors.toList());
-//
-//		List<Creature> tmpList = new ArrayList<Creature>();
-//		tmpList.addAll(newComcList);
-//		tmpList.addAll(newSpecList);
-//
-//		return new AjaxResult(1, "SUCCESS", tmpList);
-//
-//	}
 }
